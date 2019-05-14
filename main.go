3a031eed43b1a1c4b2ff367b38eea138b02cb318
yudaishimanaka <gurutaminn.h8@gmail.com>
2019-05-14 09:26:50 +0900

    git add . してしまった...

--- main.go
+++ main.go
@@ -7,26 +7,19 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/BurntSushi/toml"
 	"github.com/urfave/cli"
 )
 
-type Config struct {
-	Conf TimeproXgConfig `toml:"timepro-xg"`
-}
-
-type TimeproXgConfig struct {
-	RequestUrl string `toml:"request_url"`
-	UserId     string `toml:"user_id"`
-	Password   string `toml:"password"`
-}
+var (
+	requestUrl string
+	userId     string
+	password   string
+)
 
 func main() {
-	var conf Config
-	_, err := toml.DecodeFile("config.toml", &conf)
-	if err != nil {
-		log.Fatal(err)
-	}
+	requestUrl = os.Getenv("TIMEPROXG_REQUEST_URL")
+	userId = os.Getenv("TIMEPROXG_USERID")
+	password = os.Getenv("TIMEPROXG_PASSWORD")
 
 	app := cli.NewApp()
 	app.Name = "timepro-xg"
@@ -39,7 +32,10 @@ func main() {
 			Usage: "timepro-xg in <- Going to work!",
 			Action: func(c *cli.Context) error {
 				fmt.Printf("Do you really go to work? [y/n] :")
-				attendanceRequest(conf, "PUNCH1", "PUNCH1")
+				if err := attendanceRequest("PUNCH1", "PUNCH1"); err != nil {
+					log.Fatal(err)
+				}
+
 				if askForConfirmation() {
 					fmt.Printf("今日も一日がんばるぞい!\n")
 				}
@@ -52,7 +48,10 @@ func main() {
 			Usage: "timepro-xg out <- Leaving work!",
 			Action: func(c *cli.Context) error {
 				fmt.Printf("Do you really leaving work? [y/n] :")
-				attendanceRequest(conf, "PUNCH2", "PUNCH2")
+				if err := attendanceRequest("PUNCH2", "PUNCH2"); err != nil {
+					log.Fatal(err)
+				}
+
 				if askForConfirmation() {
 					fmt.Printf("今日も一日お疲れ様でした!\n")
 				}
@@ -65,29 +64,16 @@ func main() {
 	app.Run(os.Args)
 }
 
-func attendanceRequest(conf Config, pageStatus, process string) error {
+func attendanceRequest(pageStatus, process string) error {
 	values := url.Values{}
 	values.Add("PAGESTATUS", pageStatus)
-	values.Add("LoginID", conf.Conf.UserId)
-	values.Add("PassWord", conf.Conf.Password)
 	values.Add("PROCESS", process)
+	values.Add("LoginID", userId)
+	values.Add("PassWord", password)
 
-	req, err := http.NewRequest("POST", conf.Conf.RequestUrl, nil)
+	_, err := http.PostForm(requestUrl, values)
 	if err != nil {
 		return err
-	}
-
-	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
-	req.Header.Add("Accept-Language", `ja,en-US;q=0.7,en;q=0.3`)
-	req.Header.Add("Referer", conf.Conf.RequestUrl)
-	req.Header.Add("Content-Type", `application/x-www-form-urlencoded`)
-	req.Header.Add("Connection", `keep-alive`)
-	req.Header.Add("Upgrade-Insecure-Request", `1`)
-
-	req.URL.RawQuery = values.Encode()
-	_, err2 := http.DefaultClient.Do(req)
-	if err2 != nil {
-		return err2
 	}
 
 	return nil
